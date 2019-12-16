d84f171857032b47b6250d8dabc565f0b84fd8b1
Rildo Rene Gomez Zegarra <rgomez@belatrix.com>
2019-12-16 10:47:20 -0500

    testing pipeline aws

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: ok!");
+  res.status('200').send("Status: ok! test OdLir");
 });
 
 // retrieve all musicians from data store
